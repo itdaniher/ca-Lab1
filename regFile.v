b584958b81b8498184ea5a5c26c094fbde588893
Ian Daniher <it.daniher@gmail.com>
2012-10-14 16:43:15 -0400

    updated DECODER with Slater's enable code

--- regFile.v
+++ regFile.v
@@ -1,12 +1,19 @@
 module DECODER(OUT, IN, ENABLE);
  
-	output [31:0] OUT;
-	input [4:0] IN;
-	input ENABLE;
-
-	assign OUT = 1 << IN;
-endmodule
+  output [31:0] OUT;
+  reg [31:0] OUT;
+  input [4:0] IN;
+  input ENABLE;
  
+  always @ (ENABLE) begin
+    case (ENABLE)
+      0 : OUT = 0;
+      1 : OUT = 1 << IN;
+    endcase
+  end
+ 
+endmodule
+
 module BIGMUX(CTRL, in0, in1, in2, in3, in4, in5, in6, in7, in8, in9, in10, in11, in12, in13, in14, in15, in16, in17, in18, in19, in20, in21, in22, in23, in24, in25, in26, in27, in28, in29, in30, in31, out);
  
 	output [31:0] out;
