fcce94a9af71212bb83dda99d99695edcd2c67fc
Ian Daniher <it.daniher@gmail.com>
2012-10-15 14:52:24 -0400

    sveltify python test code a bit more

--- regFileTest.py
+++ regFileTest.py
@@ -9,18 +9,16 @@ for line in open("data.txt").read().splitlines():
 registers = 32*[32*'x']
 
 for line in lines:
+	regCt = int(line[0][1])
 	if line[0][0] == "wreg":
-		registers[int(line[0][1])] = line[1]
+		registers[regCt] = line[1]
 		registers[0] = 32*'0'
 	else:
-		try:
-			assert registers[int(line[0][1])] == line[1]
-		except:
-			try:
-				assert registers[int(line[0][1])] == 32*'x'
-			except:
+		if (registers[regCt] != line[1]):
+			if registers[regCt] != 32*'x':
 				print "FAIL"
 				print line[1]
-				print registers[int(line[0][1])]
+				print registers[regCt]
 				quit()
+
 print "PASS"
